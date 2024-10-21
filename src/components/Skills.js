7ea8f07a3cf7e2ef5cd84786e02b51d5648d3f56
Thiagoocosta86 <thiagoocosta86@gmail.com>
2024-10-21 15:55:11 +0100

    Applyed a standard feature to 'Skills' container, which through a basic button (for now), it is possible to switch from soft to hard skills.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,42 +13,49 @@ const Skills = () => {
     <section className="skills">
       <h2>Skills</h2>
 
-      <div className={`skills-container ${currentPanel === 1 ?'shift':''}`}>
-        <div className="skills-column soft-skills-panel">
-          <h3>Soft Skills</h3>
-          <ul className="soft-skills">
-            <li>Team Player</li>
-            <li>Resilient</li>
-            <li>Hard-Worker</li>
-            <li>#</li>
-            <li>#</li>
-          </ul>
-        </div>
+      <div className= "skills-container">
+        {currentPanel === 0 && (
+          <div className= "skills-column soft-skills-panel" >
+            <h3>Soft Skills</h3>
+            <ul className="soft-skills">
+              <li>Team Player</li>
+              <li>Resilient</li>
+              <li>Hard-Worker</li>
+              <li>#</li>
+              <li>#</li>
+            </ul>
+          </div>
+        )}
 
-        <div className="skills-column hard-skills-panel">
-          <h3>Frontend</h3>
-          <ul className="frontend-skills">
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>Vue.js</li>
-          </ul>
-        </div>
+        {currentPanel === 1 && (
+          <div className= "skills-column hard-skills-panel">
+            <div className="frontend-column">
+              <h3>Frontend</h3>
+              <ul className="frontend-skills">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Vue.js</li>
+              </ul>
+            </div>
+          
 
-        <div>
-          <h3>Backend</h3>
-          <ul className="backend-skills">
-            <li>Node.js</li>
-            <li>Express</li>
-            <li>MongoDB</li>
-            <li>MySQL</li>
-            <li>PHP</li>
-            <li>Java</li>
-          </ul>
-        </div>
+            <div className="backend-column">
+              <h3>Backend</h3>
+              <ul className="backend-skills">
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>MongoDB</li>
+                <li>MySQL</li>
+                <li>PHP</li>
+                <li>Java</li>
+              </ul>
+            </div>
+          </div>  
+        )}
       </div>
-
+    
       <button className="toggle-btn" onClick={handleNextPanel}>
         {currentPanel === 0 ? 'Next →' : '← Back'}
       </button>
