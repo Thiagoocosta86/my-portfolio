c8a48f95ad085e194d3a12fbd56a205997230124
Thiagoocosta86 <thiagoocosta86@gmail.com>
2024-11-07 16:00:00 +0000

    1- started working on icons and progress-bar for each tech skills. 2- frontend icons included.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import { ReactComponent as Html } from '../assets/icons/html-colour.svg';
+import { ReactComponent as Css } from '../assets/icons/css-colour.svg';
+import { ReactComponent as JavaScript } from '../assets/icons/javascript-colour.svg';
+import { ReactComponent as ReactIcon } from '../assets/icons/react-colour.svg';
+
 
 const Skills = () => {
   const [currentTab, setCurrentTab] = useState(0);
@@ -30,11 +35,35 @@ const Skills = () => {
               <div className="frontend-column">
                 <h4>Frontend</h4>
                 <ul className="frontend-skills">
-                  <li>HTML</li>
-                  <li>CSS</li>
-                  <li>JavaScript</li>
-                  <li>React</li>
-                  <li>Vue.js</li>
+                  <li>
+                    <Html title="HTML" width="24" height="24" alt="HTML Icon" className="tech-icon" />
+                    HTML
+                    <div className="knowledge-level">
+                      <div className="progress-bar" style={{ width: '85%' }}></div>
+                    </div>
+                  </li>
+                  <li>
+                    <Css title="CSS" width="24" height="24" alt="CSS Icon" className="tech-icon" />
+                      CSS
+                    <div className="knowledge-level">
+                      <div className="progress-bar" style={{ width: '45%' }}></div>
+                    </div>
+                  </li>
+                  <li>
+                    <JavaScript title="JavaScript" width="24" height="24" alt="JavaScript Icon" className="tech-icon" />
+                      JavaScript
+                    <div className="knowledge-level">
+                      <div className="progress-bar" style={{ width: '40%' }}></div>
+                    </div>
+                  </li>
+                  <li>
+                    <ReactIcon title="React" width="24" height="24" alt="React Icon" className="tech-icon" />
+                      React
+                    <div className="knowledge-level">
+                      <div className="progress-bar" style={{ width: '35%' }}></div>
+                    </div>
+                  </li>
+                  
                 </ul>
               </div>
 
