0ed66823c03e3e708985a3e6ba5594b408917326
Thiagoocosta86 <thiagoocosta86@gmail.com>
2024-11-10 00:48:57 +0000

    included a sample text in AboutText.js. Later will change the text.

--- src/data/aboutText.js
+++ src/data/aboutText.js
@@ -1,5 +1,11 @@
 const aboutText = 
-    'sample'
+    `
+    Hi! I'm Thiago, an IT student at CCT College Dublin.
+    Over the past three years, I've been challenging myself in the field of Information Technology. 
+    From learning programming fundamentals to exploring database management, networking, and other things, I have tried to gain a strong base in IT.
+    I've also sought practical experience through online courses.
+    Now, as I approach the end of my studies, I am excited to apply my knowledge and skills and make a positive impact in the IT industry. 
+`
 ;
 
 export default aboutText;
