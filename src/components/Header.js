62e8a05cec8e7f325ba3f3a3698d50dbe4a0a640
Thiagoocosta86 <thiagoocosta86@gmail.com>
2024-12-07 13:15:50 +0000

    1- created an animation for the logo.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <div className="logo">THIAGO COSTA</div>
+      <div className="logo">
+        <svg viewBox='0 0 1000 160'>
+          <text x="50%" y="50%" dy=".32em" textAnchor="middle" className="text-body">
+            THIAGO COSTA
+          </text>
+        </svg>
+        
+      </div>
       <nav id="navbar">
         <ul>
           
