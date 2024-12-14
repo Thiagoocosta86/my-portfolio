53ee876ba4937d041feb5229a142aa59bbd484c0
Thiagoocosta86 <thiagoocosta86@gmail.com>
2024-12-14 01:21:49 +0000

    1- creating a responsive menu for different devices, right now working on 768px device.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header>
       <div className="logo">
-        <svg viewBox='0 0 1000 160'>
+        <svg viewBox="0 0 1000 160">
           <text x="50%" y="50%" dy=".32em" textAnchor="middle" className="text-body">
             THIAGO COSTA
           </text>
         </svg>
-        
       </div>
-      <nav id="navbar">
+      <div className="hamburger" onClick={toggleMenu}>
+        ☰
+      </div>
+      <nav id="navbar" className={isMenuOpen ? 'open' : ''}>
         <ul>
-          
+          {isMenuOpen && (
+              <li><a href="#home">Home</a></li>
+            )}
           <li><a href="#skills">Skills</a></li>
           <li><a href="#certificates">Certificates</a></li>
           <li><a href="#projects">Projects</a></li>
-          
+          {isMenuOpen && (
+              <li><a href="#contact">Contact</a></li>
+            )}
         </ul>
       </nav>
     </header>
