c9a37f100b4a71b2216efc00f53d8959baadc850
Thiagoocosta86 <thiagoocosta86@gmail.com>
2024-12-17 11:38:50 +0000

    1- changed the responsiviness of the navbar when resizing, there was a bug that on resizing back from mobile to desktop the mobile menu would still appear.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobileView, setIsMobileView] = useState(window.innerWidth < 768);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const handleResize = () => {
+    const isMobile = window.innerWidth < 768;
+    setIsMobileView(isMobile);
+    if (!isMobile) {
+      setIsMenuOpen(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <header>
+    <header >
       <div id="home" className="logo">
         <svg viewBox="0 0 1000 160">
           <text x="50%" y="50%" dy=".32em" textAnchor="middle" className="text-body">
@@ -21,7 +37,7 @@ const Header = () => {
       </div>
       <nav id="navbar" className={isMenuOpen ? 'open' : ''}>
         <ul>
-          {isMenuOpen && (
+          {isMenuOpen && isMobileView && (
               <li><a href="#home">Home</a></li>
             )}
           <li><a href="#skills">Skills</a></li>
