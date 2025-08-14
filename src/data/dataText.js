9c3083ab9b430966791ed86c00ce6561d80db506
Thiagoocosta86 <thiagoocosta86@gmail.com>
2025-08-14 12:55:16 +0100

    1 - Changed hero greeting; 2 - Changed webpage pallet colour orientation; 3 - Made the right adjustments for medias to apply new colours.

--- src/data/dataText.js
+++ src/data/dataText.js
@@ -1,15 +1,16 @@
 const aboutText = 
     `
-    <p>Hello there!</p>
-    <p>I'm a tech enthusiast, with a strong foundation in Computer Science, based in Dublin, Ireland. 
-  With a passion for innovation and problem-solving, I’ve delved deep into the world of technology, graduating 
-  with a solid academic background and I've also sought practical experience through online courses.</p>
+    <p>Hello there, I'm Thiago.</p>
+    <p>I'm a tech enthusiast based in Dublin, Ireland.</p>
+    <p>With a strong foundation in Computer Science and 
+  a passion for innovation and problem-solving, I’ve delved deep into the world of technology, graduating 
+  with a solid academic background and I’ve also sought practical experience through online courses.</p>
   
     <p>Currently, I’m channeling my expertise into cloud computing, exploring its limitless potential to drive 
   efficiency and scalability. I thrive on learning about cutting-edge tools, architecting solutions, and 
   optimizing workflows in the cloud space.</p>
   
-    <p>Outside of tech, I enjoy spending some quality time with family and friends, and I’m always open to 
+    <p>Outside of tech, I enjoy spending quality time with family and friends, and I’m always open to 
   connecting with like-minded individuals to share knowledge and experiences.</p>
   
     <p>Let’s build something amazing together!</p>
